22c6e1244c5b882cf1d3209fdd7bb5cdd2944b15
Nícolas do Couto Chaves <100481997+nicolascoutochaves@users.noreply.github.com>
2023-05-25 23:00:52 -0300

    First commit

---
+++ semana2/separadordenotas.c
@@ -0,0 +1,6 @@
+/* Esse programa recebe um valor e calcula quantas cédulas devem ser entregues de para preencher esse valor */
+#include <stdio.h>
+int main(){
+    printf("Teste!");
+    return 0;
+}
