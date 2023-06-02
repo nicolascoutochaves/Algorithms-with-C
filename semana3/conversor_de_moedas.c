b538f89e62feec9181b36d526d49ecaff7f038d4
nicolascoutochaves <nicolasdocoutochaves@gmail.com>
2023-06-02 14:40:19 -0300

    Atualizado o exercicio conversor_de_moedas.c
    conversor de moedas agora esta funcionando conforme o esperado

--- semana3/conversor_de_moedas.c
+++ semana3/conversor_de_moedas.c
@@ -13,28 +13,63 @@ int main(){
     float montante, resultado;
     char moedaorigem, moedadestino;
     printf("Conversor de moedas\nDigite a moeda de origem (use R/r para real, D/d para dolar e E/e para euro): ");
-    moedaorigem = tolower(moedaorigem);
     scanf(" %c", &moedaorigem);
+    moedaorigem = tolower(moedaorigem);
 
-    printf("Digite a moeda de destino (use R/r para real, D/d para dolar e E/e para euro)\n");
-    moedadestino = tolower(moedadestino);
+    printf("Digite a moeda de destino (use R/r para real, D/d para dolar e E/e para euro) ");
     scanf(" %c", &moedadestino);
+    moedadestino = tolower(moedadestino);
 
     printf("Digite o montante em ");
     switch (moedaorigem){
         case 'r':
-            printf("Real: ");
-            break;
+            printf("Real: R$");
+	    scanf("%f", &montante);
+	    
+	    if (moedadestino == 'd'){
+		resultado =  montante * RUS;
+		printf("Seu montante em dolares e de U$%.2f\n", resultado);
+	    } else if (moedadestino == 'e'){
+		resultado = montante * REUR;
+                printf("Seu montante em euros e de £%.2f\n", resultado);
+	    } else{
+	    printf("Impossivel converter para essa moeda!\n");
+	    }
+	    break;
+		  
         case 'd':
-            printf("Dolar: ");
+            printf("Dolar: U$");
+	    scanf("%f", &montante);
+
+	    if (moedadestino == 'r'){
+                resultado = montante / RUS;
+                printf("Seu montante em real e de R$%.2f\n", resultado);
+            } else if (moedadestino == 'e'){
+                resultado = montante / RUS * REUR;
+                printf("Seu montante em dolares e de U$%.2f\n", resultado);
+            } else{
+            printf("Impossivel converter para essa moeda!\n");
+            }
+
             break;
         case 'e':
-            printf("Euro: ");
-        break;
-        default: printf("\n[Erro]\nDigite uma moeda valida!n");
-    }
-    scanf("%f", &montante);
+            printf("Euro: £");
+	    scanf("%f", &montante);
 
+	    if (moedadestino == 'd'){
+                resultado = montante / REUR * RUS;
+                printf("Seu montante em dolares e de U$%.2f\n", resultado);
+            } else if (moedadestino == 'r'){
+                resultado = montante / REUR;
+                printf("Seu montante em real e de R$%.2f\n", resultado);
+            } else{
+            printf("Impossivel converter para essa moeda!\n");
+            }
+
+        break;
+        default: printf("\n[Erro]\nDigite uma moeda valida!\n");
+    }
+    
 
     return 0;
 }
