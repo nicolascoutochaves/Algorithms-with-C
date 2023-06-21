82875806b5a601ef0189b9c0f11139c818682f1d
nicolascoutochaves <nicolasdocoutochaves@gmail.com>
2023-06-21 09:47:22 -0300

    made a very cool algorithm that makes the Sum of digits

--- week5/9digit_adder.c
+++ week5/9digit_adder.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 int main(){
+    int number;
+    int sum = 0, digit;
 
+    printf("Enter a number: ");
+    scanf("%d", &number);
 
+    while (number > 0)
+    {
+        digit = number % 10;
+        //printf("\nDigit = %d\n", digit);
+
+        //printf("%d + %d = ", sum, digit);
+        sum += digit;
+        //printf("%d\n", sum);
+
+        number /= 10;
+        //printf("Number = %d\n", number);
+    }
+    
+    printf("\nThe sum is = %d\n", sum);
     return 0;
 }
