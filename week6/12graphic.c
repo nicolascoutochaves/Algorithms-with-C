ab2d419e3522f0deab40711fe96acf6adc40b32e
nicolascoutochaves <nicolasdocoutochaves@gmail.com>
2023-07-05 15:09:15 -0300

    created a program that prints a '*' according to a vector given by the user

--- week6/12graphic.c
+++ week6/12graphic.c
@@ -1,6 +1,25 @@
+/* This program prints a '*' according to a vector given by the user */
 #include <stdio.h>
-int main(){
+#define MAX 5
 
+int main(){
+    int n[MAX] = {};
+    int i, k;
+
+    for (i = 0; i < MAX; i++){
+        printf("\nEnter the value %d: ", i+1);
+        scanf("%d", &n[i]);
+    }
+
+    for (i = 0; i < MAX; i++){
+
+        for (k = 0; k < n[i]; k++){
+
+            printf("*");
+        }
+        printf("\n");
+
+    }
 
     return 0;
 }
