25918294c1cddbd247888abb8a4ad1b802dde637
nicolascoutochaves <nicolasdocoutochaves@gmail.com>
2023-07-17 14:01:42 -0300

    implemented strlen and strcmp functions

--- week7/3strlen.c
+++ week7/3strlen.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#define MAX_CHAR 30
 int main(){
+    char s[MAX_CHAR+1] = {0};
+    int i = 0, c = 0;
+    printf("Enter the string: ");
+    scanf("%s", s);
 
+    while(s[i] != '\0'){
+        c++;
+        i++;
+    }
 
+    
+    printf("length of '%s' = %d\n", s, c);
+    
+    
     return 0;
 }
--- week7/4strcmp.c
+++ week7/4strcmp.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#define MAX_CHAR 30
 int main(){
+    char s1[MAX_CHAR+1] = {0}, s2[MAX_CHAR+1] = {0};
+    int i = 0, j = 0;
+    printf("Enter the string 1: ");
+    scanf("%s", s1);
+    printf("Enter the string 2: ");
+    scanf("%s", s2);
+
+    do{
+        if (s1[i] > s2[i]){
+            j++;
+        } else if (s1[i] < s2[i]){
+            j--;
+        }
+        i++;
+    } while(s1[i] != '\0' || s2[i] != '\0');
 
 
+    if(j > 0){
+        printf("s1 > s2\n");
+        printf("%d\n", j);
+    } else if (j < 0){
+        printf("s1 < s2\n");
+        printf("%d\n", j);
+    } else{
+        printf("s1 == s2\n");
+    }
+    
+    
+   
+
+
+    
+    
     return 0;
 }
