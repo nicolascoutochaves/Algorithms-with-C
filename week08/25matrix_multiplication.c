d75dd5c0b7b048f4c135b48d9f18e780f511d8de
nicolascoutochaves <nicolasdocoutochaves@gmail.com>
2023-10-11 13:26:43 -0300

    made an algorithm that multiplicates two mxn matrices

--- week08/25matrix_multiplication.c
+++ week08/25matrix_multiplication.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "matrix.c"
+typedef struct{
+    int res[ROWS][COLUMNS];
+}result;
+
+result multiplicateMatrix(int m1[ROWS][COLUMNS], int m2[ROWS][COLUMNS]){
+    int i, j, k, l;
+    int tmp;
+    result result;
+   
+    for(i = 0; i < ROWS; i++){
+        for(j = 0; j < COLUMNS; j++){
+            tmp = 0;
+            for(k = 0; k < COLUMNS; k++){
+                
+                tmp += m1[i][k] * m2[k][j];
+            }
+            result.res[i][j] = tmp;
+        }
+
+    }
+    return result;
+}
 int main(){
+    int m1[ROWS][COLUMNS], m2[ROWS][COLUMNS];
+    result result;
+    populateMatrix(m1);
+    populateMatrix(m2);
+    printMatrix(m1);
+    printMatrix(m2);
+    result = multiplicateMatrix(m1, m2);
+    printMatrix(result.res);
 
 
     return 0;
---
+++ week08/matrix.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ROWS 5
+#define COLUMNS 4
+#define MAX_RAND 10
+
+void populateVec(int v[], int length){
+    int i;
+    srand(time(NULL)+(time_t)v); //using the addres of each vec to avoid same values
+    for(i = 0; i < length; i++){
+        
+        v[i] = rand() % MAX_RAND;
+        
+    }
+}
+void populateMatrix(int m[ROWS][COLUMNS]){
+    int i, j;
+    srand(time(NULL)+(time_t)m);
+    for(i = 0; i < ROWS; i++){
+        for(j = 0; j < COLUMNS; j++){
+            m[i][j] = rand() % MAX_RAND;
+        }
+    }
+}
+
+
+void printMatrix(int m[ROWS][COLUMNS]){
+    int i, j;
+
+    for(j = 0; j < COLUMNS; j++){
+        printf("%5d", j+1);
+    }
+    printf("\n\n");
+    for(i = 0; i < ROWS; i++){
+        printf("%d", i+1);
+        
+        for(j = 0; j < COLUMNS; j++){
+            printf("%5d", m[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void printVec(int v[], int length){
+    int i;
+    for(i = 0; i < length; i++){
+        printf("%5d", v[i]);
+    }
+    printf("\n");
+}
+
+void composeVector(int m[ROWS][COLUMNS], int v1[], int length){
+    int i, j, c;
+    for(i = 0; i < length; i++){
+        c = 0;
+        for(j = 0; j < COLUMNS; j++){
+            c += m[i][j];
+        }
+        v1[i] = c;
+    }
+}
